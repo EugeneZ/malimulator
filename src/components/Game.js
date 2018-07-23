288a4b2c487e7f566628735cad89151d3637f637
Eugene Zaretskiy <eugene.zar@gmail.com>
2018-07-22 22:12:47 -0700

    Cleaning up types

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { type GameState } from '../game/types';
-import { newGame, inputEntered } from '../game/actionCreators';
+import { newGame, inputEntered, type Action } from '../game/actionCreators';
 import Screen from './Screen';
 import Input from './Input';
 import Messages from './Messages';
@@ -10,7 +10,7 @@ import HorizontalSplitter from './HorizontalSplitter';
 import { type Dispatch } from 'redux';
 
 export default connect((state: GameState) => state)(
-  class Game extends PureComponent<{ dispatch: Dispatch<*> } & GameState> {
+  class Game extends PureComponent<{ dispatch: Dispatch<Action> } & GameState> {
     componentDidMount() {
       this.props.dispatch(newGame());
     }
